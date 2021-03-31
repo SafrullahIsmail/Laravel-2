779ed6a249702f9243ce4f79c3aa117db942bca2
SafrullahIsmail <0821smska@gmail.com>
2021-03-31 10:14:07 +0700

    feature:first commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'PublicController@index')->name('index');
+Route::get('post/{id}', 'PublicController@singlePost')->name('singlePost');
+Route::get('about', 'PublicController@about')->name('about');
+Route::get('contact', 'PublicController@contact')->name('contact');
+
+Route::post('contact', 'PublicController@contactPost')->name('contactPost');
+
+Auth::routes();
+
+Route::get('/dashboard', 'HomeController@index')->name('dashboard');
